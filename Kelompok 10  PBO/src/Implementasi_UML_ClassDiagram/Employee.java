1fdcf533e0bac984cadb328a544dde055df66498
ahmadalfarizi2017051060 <83463202+ahmadalfarizi2017051060@users.noreply.github.com>
2021-09-19 15:03:08 +0700

    Update Employee.java

--- Kelompok 10  PBO/src/Implementasi_UML_ClassDiagram/Employee.java
+++ Kelompok 10  PBO/src/Implementasi_UML_ClassDiagram/Employee.java
@@ -1,4 +1,20 @@
 package Implementasi_UML_ClassDiagram;
 
 abstract class Employee {
+  private String name;
+  private String afm;
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setAfm(String afm) {
+    this.afm = afm;
+  }
+  public String getAfm() {
+    return afm;
+  }
+  abstract int payment();
 }
