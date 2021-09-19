24c8cf264860cfa895aa06e3f7a5f5bdfc02c377
ahmadalfarizi2017051060 <83463202+ahmadalfarizi2017051060@users.noreply.github.com>
2021-09-19 14:54:53 +0700

    Update Main2.java

--- Kelompok 10  PBO/src/Implementasi_UML_ClassDiagram/Main2.java
+++ Kelompok 10  PBO/src/Implementasi_UML_ClassDiagram/Main2.java
@@ -4,7 +4,7 @@ Ahmad Al Farizi          2017051060
 Nasywa Nathania Wirawan  2017051051
 Zaki Nugraha Muhammad    09031281924045
 */
-package Implementasi_UML_ClassDiagran;
+package Implementasi_UML_ClassDiagram;
 
 public class Main2 {
     public static void main(String []args) { 
