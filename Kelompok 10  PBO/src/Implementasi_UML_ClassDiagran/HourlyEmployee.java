eb19dd10b73055c8794984114e180f6ed11bdc9b
ahmadalfarizi2017051060 <83463202+ahmadalfarizi2017051060@users.noreply.github.com>
2021-09-19 14:43:11 +0700

    Add files via upload

---
+++ Kelompok 10  PBO/src/Implementasi_UML_ClassDiagran/HourlyEmployee.java
@@ -0,0 +1,23 @@
+package Implementasi_UML_ClassDiagran;
+
+public class HourlyEmployee extends Employee {
+    private int hoursWorked;
+    private int hourlyPayment;
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+    public void setHourlyPayment(int hourlyPayment) {
+        this.hourlyPayment = hourlyPayment;
+    }
+    public int getHourlyPayment() {
+        return hourlyPayment;
+    }
+    @Override
+    public int payment() {
+        return hoursWorked*hourlyPayment;
+    }
+}
---
+++ Kelompok 10  PBO/src/Implementasi_UML_ClassDiagran/SalariedEmployee.java
@@ -0,0 +1,16 @@
+package Implementasi_UML_ClassDiagran;
+
+public class SalariedEmployee extends Employee {
+    private int salary;
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    @Override
+    public int payment() { 
+        return salary;
+    }
+}
