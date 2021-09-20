48db2b4f4ace079cf5a732831574afad02caa530
ZakiNugrahaM <67621234+Zaki27@users.noreply.github.com>
2021-09-20 20:44:33 +0700

    Update SalariedEmployee.java

--- Kelompok 10  PBO/src/Implementasi_UML_ClassDiagram/SalariedEmployee.java
+++ Kelompok 10  PBO/src/Implementasi_UML_ClassDiagram/SalariedEmployee.java
@@ -1,4 +1,15 @@
 package Implementasi_UML_ClassDiagram;
 
 public class SalariedEmployee extends Employee {
-}
+    private int salary;
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    @Override
+    public int payment() { 
+        return salary;
+    }
